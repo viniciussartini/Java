621ee2ec16e7ed5cb148e00bcbdf79486466c31f
Vinicius Sartini <sartini.vinicius@outlook.com>
2024-03-09 11:04:35 -0300

    Resolving the problem without OOP

--- OOP_01/src/main/java/com/mycompany/oop_01/OOP_01.java
+++ OOP_01/src/main/java/com/mycompany/oop_01/OOP_01.java
@@ -1,8 +1,36 @@
 package com.mycompany.oop_01;
 
+import java.util.Scanner;
+
 public class OOP_01 {
 
     public static void main(String[] args) {
+        Scanner inputKeyboard = new Scanner(System.in);
         double xA, xB, xC, yA, yB, yC;
+        
+        System.out.println("Enter the measures of triangle X: ");
+        xA = inputKeyboard.nextDouble();
+        xB = inputKeyboard.nextDouble();
+        xC = inputKeyboard.nextDouble();
+        
+        System.out.println("Enter the measures of triangle Y:");
+        yA = inputKeyboard.nextDouble();
+        yB = inputKeyboard.nextDouble();
+        yC = inputKeyboard.nextDouble();
+        
+        double p = (xA + xB+ xC)/2.0;
+        double areaX = Math.sqrt(p * (p - xA) * (p - xB) * (p - xC));
+        
+        p = (yA + yB+ yC)/2.0;
+        double areaY = Math.sqrt(p * (p - yA) * (p - yB) * (p - yC));
+        
+        System.out.printf("Triangle X area: %.4f%n", areaX);
+        System.out.printf("Triangle Y area: %.4f%n", areaY);
+        
+        if (areaX > areaY) {
+            System.out.println("Larger area: X");
+        } else {
+            System.out.println("Larger area: Y");
+        }
     }
 }
