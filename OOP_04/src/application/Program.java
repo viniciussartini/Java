b2a2ea703affed2094eacace2495994a456cf2b9
Vinicius Sartini <sartini.vinicius@outlook.com>
2024-03-09 16:13:04 -0300

    Solving the problem OOP_04
    Create the application and the class

--- OOP_04/src/application/Program.java
+++ OOP_04/src/application/Program.java
@@ -1,10 +1,27 @@
 package application;
 
+import java.util.Scanner;
+
+import entities.Rectangle;
+
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner inputKeyboard = new Scanner(System.in);
+		Rectangle rectangle = new Rectangle();
+		
+		System.out.println("Enter rectangle width: ");
+		rectangle.width = inputKeyboard.nextDouble();
+		
+		System.out.println("Enter rectangle height");
+		rectangle.height = inputKeyboard.nextDouble();
+		
+		System.out.println("Area: " + rectangle.area());
+		System.out.println("Perimeter: " + rectangle.perimeter());
+		System.out.println("Diagonal: " + rectangle.diagonal());
+		
+		inputKeyboard.close();
 	}
 
 }
