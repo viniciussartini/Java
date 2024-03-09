cea214cee46d95d5258aee1ae89be77f9a01440c
Vinicius Sartini <sartini.vinicius@outlook.com>
2024-03-09 15:04:52 -0300

    Solution for exercise OOP_03

--- OOP_03/src/application/Program.java
+++ OOP_03/src/application/Program.java
@@ -1,10 +1,33 @@
 package application;
 
+import java.util.Scanner;
+
+import entities.Employee;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner inputKeyboard = new Scanner(System.in);
+		
+		Employee employee = new Employee();
+		
+		System.out.println("Name: ");
+		employee.name = inputKeyboard.nextLine();
+		System.out.println("Gross Salary: ");
+		employee.grossSalary = inputKeyboard.nextDouble();
+		System.out.println("Tax: ");
+		employee.tax = inputKeyboard.nextDouble();
+		
+		System.out.println("Employee: " + employee);
+		
+		System.out.println("Which percentage to increase salary: ");
+		employee.increaseSalary(inputKeyboard.nextDouble());
+		
+		System.out.println("Update: " + employee);
+		
+		inputKeyboard.close();
+		
 	}
 
 }
