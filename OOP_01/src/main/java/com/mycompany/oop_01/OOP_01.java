5d8e64dd6bbac644335a214f3db2c35904942448
Vinicius Sartini <sartini.vinicius@outlook.com>
2024-03-09 11:40:00 -0300

    applying OOP to solve the problem

--- OOP_01/src/main/java/com/mycompany/oop_01/OOP_01.java
+++ OOP_01/src/main/java/com/mycompany/oop_01/OOP_01.java
@@ -1,28 +1,29 @@
 package com.mycompany.oop_01;
 
+import com.mycompany.oop_01.entities.Triangle;
 import java.util.Scanner;
 
 public class OOP_01 {
 
     public static void main(String[] args) {
         Scanner inputKeyboard = new Scanner(System.in);
-        double xA, xB, xC, yA, yB, yC;
+        
+        Triangle x, y;
+        x = new Triangle();
+        y = new Triangle();
         
         System.out.println("Enter the measures of triangle X: ");
-        xA = inputKeyboard.nextDouble();
-        xB = inputKeyboard.nextDouble();
-        xC = inputKeyboard.nextDouble();
+        x.a = inputKeyboard.nextDouble();
+        x.b = inputKeyboard.nextDouble();
+        x.c = inputKeyboard.nextDouble();
         
         System.out.println("Enter the measures of triangle Y:");
-        yA = inputKeyboard.nextDouble();
-        yB = inputKeyboard.nextDouble();
-        yC = inputKeyboard.nextDouble();
+        y.a = inputKeyboard.nextDouble();
+        y.b = inputKeyboard.nextDouble();
+        y.c = inputKeyboard.nextDouble();
         
-        double p = (xA + xB+ xC)/2.0;
-        double areaX = Math.sqrt(p * (p - xA) * (p - xB) * (p - xC));
-        
-        p = (yA + yB+ yC)/2.0;
-        double areaY = Math.sqrt(p * (p - yA) * (p - yB) * (p - yC));
+        double areaX = x.area();
+        double areaY = y.area();
         
         System.out.printf("Triangle X area: %.4f%n", areaX);
         System.out.printf("Triangle Y area: %.4f%n", areaY);
