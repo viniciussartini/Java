5537459d6789ad7a0053a400ec01931857691dcb
Vinicius Sartini <sartini.vinicius@outlook.com>
2024-03-09 17:02:13 -0300

    Updated Program file

--- OOP_05/src/application/Program.java
+++ OOP_05/src/application/Program.java
@@ -1,10 +1,31 @@
 package application;
 
+import java.util.Scanner;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner inputKeyboard = new Scanner(System.in);
+		Student student = new Student();
+		
+		System.out.println("Enter the student name: ");
+		student.name = inputKeyboard.nextLine();
+		System.out.println("Enter the first grade: ");
+		student.grade1 = inputKeyboard.nextDouble();
+		System.out.println("Enter the second grade: ");
+		student.grade2 = inputKeyboard.nextDouble();
+		System.out.println("Enter the third grade: ");
+		student.grade3 = inputKeyboard.nextDouble();
+		
+		if (student.finalGrade() > 60) {
+			System.out.println("Final Grade: " + student.finalGrade() + " PASS");
+		} else {
+			System.out.println("Final Grade: " + student.finalGrade() + " FAILED");
+			System.out.println("Missing " + student.missingPoints() + "POINTS");
+		}
+		
+		inputKeyboard.close();
 	}
 
 }
